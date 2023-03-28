44ca644a7227ab15ed284ce91c90a68730c60eed
Oleksii <olekssokol@gmail.com>
2023-03-28 21:09:18 +0300

    Created template and AddData view

---
+++ Hotel/Model/DataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Model
+{
+    public class DataModel
+    {
+        private readonly List<DataModel> _hotels;
+
+        public DataModel()
+        {
+            _hotels = new List<DataModel>();
+        }
+
+        public IEnumerable<DataModel> GetHotels()
+        {
+            return _hotels;
+        }
+    }
+}
--- Hotel/View/AddData.xaml.cs
+++ Hotel/View/AddData.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hotel
+namespace CitizenDevelopment.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddData.xaml  
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddData : Window
     {
-        public MainWindow()
+        public AddData()
         {
             InitializeComponent();
         }
